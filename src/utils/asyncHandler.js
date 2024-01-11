8a106999bed8c6b17bc942a00ad0addc34beaf3d
nisarg <zoomtest4u@gmail.com>
2024-01-11 15:31:56 +0530

    asyncHandler

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// evolution of asyncHandler function !!!! below that is written
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => {() => {}}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+// Second way to handle asyncHandler: !!! below code is there which can replace written at top :
+/*
+const asyncHandler = (fn) => async (req,res,next) => {
+    try {
+        await fn(req,res,next);
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success: false,
+            message: error.message || "Internal Server Error",
+        })
+    }
+}
+
+*/
