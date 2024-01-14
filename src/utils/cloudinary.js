4fba060a7e620f88d2c973f052a9567fe83221e1
nisarg <zoomtest4u@gmail.com>
2024-01-14 20:01:46 +0530

    sm

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,39 +1,3 @@
-// import { v2 as cloudinary } from "cloudinary";
-// import fs from "fs";
-
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
-
-// const uploadOnCloudinary = async (localFilePath) => {
-//   try {
-//     if (!localFilePath) return null;
-
-//     // Upload the file on Cloudinary
-//     const response = await cloudinary.uploader.upload(localFilePath, {
-//       resource_type: "auto",
-//     });
-
-//     // Check if the local file exists before attempting to unlink it
-//     if (fs.existsSync(localFilePath)) {
-//       fs.unlinkSync(localFilePath); // Delete the local file
-//     }
-
-//     return response;
-//   } catch (error) {
-//     // Remove the locally saved temporary file if the upload operation fails
-//     if (fs.existsSync(localFilePath)) {
-//       fs.unlinkSync(localFilePath);
-//     }
-
-//     return null;
-//   }
-// };
-
-// export { uploadOnCloudinary };
-
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
@@ -72,3 +36,39 @@ const uploadOnCloudinary = async (localFilePath) => {
 };
 
 export { uploadOnCloudinary };
+
+// import { v2 as cloudinary } from "cloudinary";
+// import fs from "fs";
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+// const uploadOnCloudinary = async (localFilePath) => {
+//   try {
+//     if (!localFilePath) return null;
+
+//     // Upload the file on Cloudinary
+//     const response = await cloudinary.uploader.upload(localFilePath, {
+//       resource_type: "auto",
+//     });
+
+//     // Check if the local file exists before attempting to unlink it
+//     if (fs.existsSync(localFilePath)) {
+//       fs.unlinkSync(localFilePath); // Delete the local file
+//     }
+
+//     return response;
+//   } catch (error) {
+//     // Remove the locally saved temporary file if the upload operation fails
+//     if (fs.existsSync(localFilePath)) {
+//       fs.unlinkSync(localFilePath);
+//     }
+
+//     return null;
+//   }
+// };
+
+// export { uploadOnCloudinary };
