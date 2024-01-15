176693b89c0bf5c2aef256b395d6d97244424544
nisarg <zoomtest4u@gmail.com>
2024-01-15 23:35:07 +0530

    playlist, subscription, and tweet models and user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,45 +1,42 @@
 import { Router } from "express";
-// import { 
-//     loginUser, 
-//     logoutUser, 
-//     registerUser, 
-//     refreshAccessToken, 
-//     changeCurrentPassword, 
-//     getCurrentUser, 
-//     updateUserAvatar, 
-//     updateUserCoverImage, 
-//     getUserChannelProfile, 
-//     getWatchHistory, 
+// import {
+//     loginUser,
+//     logoutUser,
+//     registerUser,
+//     refreshAccessToken,
+//     changeCurrentPassword,
+//     getCurrentUser,
+//     updateUserAvatar,
+//     updateUserCoverImage,
+//     getUserChannelProfile,
+//     getWatchHistory,
 //     updateAccountDetails
 // } from "../controllers/user.controller.js";
-import { 
-    registerUser, 
-} from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js"
+import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-
-const router = Router()
+const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        }, 
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),
-    registerUser
-    )
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
-// router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
 //secured routes
 
-// router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser);
 // router.route("/refresh-token").post(refreshAccessToken)
 // router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 // router.route("/current-user").get(verifyJWT, getCurrentUser)
@@ -51,4 +48,4 @@ router.route("/register").post(
 // router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 // router.route("/history").get(verifyJWT, getWatchHistory)
 
-export default router
+export default router;
